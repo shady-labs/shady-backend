b27816d8405771c52b42f27a4cd1e002f6d7ce0a
hemang <hemangpant2002@gmail.com>
2024-07-11 23:30:30 +0530

    added user typedefs to index

--- typeDefs/index.ts
+++ typeDefs/index.ts
@@ -3,12 +3,14 @@ import artistTypeDef from "./artist.typeDef.js";
 import trackTypeDef from "./track.typeDef.js";
 import genreTypeDef from "./genre.typeDef.js";
 import albumTypeDef from "./album.typeDef.js";
+import userTypeDef from "./user.typeDef.js";
 
 const mergedTypeDefs = mergeTypeDefs([
     artistTypeDef,
     trackTypeDef,
     genreTypeDef,
     albumTypeDef,
+    userTypeDef
 ]);
 
 export default mergedTypeDefs;
