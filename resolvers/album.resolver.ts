cf1bff78c24f5f820dd414470ddc0653c65e5bd4
hemang <hemangpant2002@gmail.com>
2024-07-11 18:33:49 +0530

    added album resolver

---
+++ resolvers/album.resolver.ts
@@ -0,0 +1,46 @@
+// import { artists } from "../dummyData/data.js";
+import Album from "../models/album.model.js";
+import Artist from "../models/artist.model.js";
+import Genre from "../models/genre.model.js";
+import Track from "../models/track.model.js";
+
+const albumResolver = {
+    Query: {
+        albums: async () => {
+        return Artist.find({});
+        },
+        album: async (_, { id }) => {
+        return Album.findById(id);
+        },
+    },
+    Mutation: { 
+        createAlbum(_, { input }) {
+            console.log("input: ", input)
+            return Album.create(input);
+        }
+    },
+    Album: {
+        tracks: async (parent) => {
+            try {
+                // find tracks with artistID present in array artistsID
+                const tracks = await Track.find({ albumId: parent._id });
+				return tracks;
+			} catch (err) {
+				console.log("Error in user.tracks resolver: ", err);
+				throw new Error(err.message || "Internal server error");
+			}
+        },
+        genres: async (parent) => {
+            try {
+                // find genres with artistID present in array artistsID
+                const genres = await Genre.find({ name: parent.genres });
+                return genres;
+            } catch (err) {
+                console.log("Error in user.genres resolver: ", err);
+                throw new Error(err.message || "Internal server error");
+            }
+        }
+    }
+};
+
+export default albumResolver;
