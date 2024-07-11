b1b989775496bb830e6dec8466ed59d1fd44251f
hemang <hemangpant2002@gmail.com>
2024-07-11 18:35:07 +0530

    updated resolver index and typedef index

--- resolvers/index.ts
+++ resolvers/index.ts
@@ -2,11 +2,13 @@ import { mergeResolvers } from "@graphql-tools/merge";
 import artistResolver from "./artist.resolver.js";
 import trackResolver from "./track.resolver.js";
 import genreResolver from "./genre.resolver.js";
+import albumResolver from "./album.resolver.js";
 
 const mergedResolvers = mergeResolvers([
     artistResolver,
     trackResolver,
-    genreResolver
+    genreResolver,
+    albumResolver,
 ]);
 
 export default mergedResolvers;
